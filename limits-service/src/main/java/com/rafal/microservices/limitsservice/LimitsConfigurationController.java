1afef3bf0e94492083f0f0688ec7ab4f7f8a3c19
rafalpasiut <rafalpasiut@gmail.com>
2018-03-14 21:54:36 +0100

    hystrix example

--- limits-service/src/main/java/com/rafal/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/rafal/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.rafal.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.rafal.microservices.limitsservice.bean.LimitsConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,5 +19,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitsConfiguration retreiveLimitsFromConfiguration(){
         return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "getDefaultConfiguration")
+    public LimitsConfiguration retreiveConfiguration(){
+        throw new RuntimeException();
+    }
+
+    private LimitsConfiguration getDefaultConfiguration(){
+        return new LimitsConfiguration(0,0);
     }
 }
--- limits-service/src/main/java/com/rafal/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/rafal/microservices/limitsservice/LimitsServiceApplication.java
@@ -4,9 +4,11 @@ import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableHystrix
 @EnableDiscoveryClient
 public class LimitsServiceApplication {
 
